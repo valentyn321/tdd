9e5ad65cb8bf488ed202835d9da04d258d9c5963
Valentyn <valentyncherkasov24@gmail.com>
2020-05-28 13:39:51 +0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -21,12 +21,10 @@ class NewVisitorClass(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-do', header_text)
-        self.fail('Finish the test!')  
-
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_tag_id('id_new_item')
-        self.assertIn(
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter here, what you have to do...'
         )
@@ -40,10 +38,11 @@ class NewVisitorClass(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_tag_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = self.browser.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            'New to-do item didn`t appear in table.'
         )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
