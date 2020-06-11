1bfba30173d80f3001ac12a29ba8ec0b1b956a19
Valentyn <valentyncherkasov24@gmail.com>
2020-06-11 18:21:38 +0300

    Add new model of list, refactored tests and views.

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -12,16 +12,24 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed('home.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     
     def test_saving_and_returning_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "First_item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second_item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         qs = Item.objects.all()
         self.assertEqual(qs.count(), 2)
@@ -29,7 +37,9 @@ class ItemModelTest(TestCase):
         first_item_from_qs = qs[0]
         second_item_from_qs = qs[1]
         self.assertEqual(first_item_from_qs.text, 'First_item')
+        self.assertEqual(first_item_from_qs.list, list_)
         self.assertEqual(second_item_from_qs.text, 'Second_item')
+        self.assertEqual(second_item_from_qs.list, list_)
 
 class ListViewTest(TestCase): 
 
@@ -37,8 +47,9 @@ class ListViewTest(TestCase):
         client = Client()
 
     def test_display_all_list_items(self):
-        Item.objects.create(text='Item 1')
-        Item.objects.create(text='Item 2')
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='Item 1')
+        Item.objects.create(list=list_, text='Item 2')
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
